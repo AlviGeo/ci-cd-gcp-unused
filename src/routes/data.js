747b17cfa475881e7cb5a7044b90dea3cf624724
Fajar Fitrianto <62609399+plotok11@users.noreply.github.com>
2022-05-13 23:58:42 +0700

    Add get hotel by id route
    adding
    router.get('/hotel-data/:id', dataController.getHotelById)

--- src/routes/data.js
+++ src/routes/data.js
@@ -4,6 +4,5 @@ const dataController = require("../controllers/hotel/hotel-data-controller");
 const router = express.Router();
 
 router.get('/hotel-data', dataController.getAllHotels);
-
-
+router.get('/hotel-data/:id', dataController.getHotelById)
 module.exports = router;
