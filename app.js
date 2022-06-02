e78016d5a0caecdc4cd362f4b1deb23b750fd942
Kelvin Ding <70010690+kelvin-ding@users.noreply.github.com>
2022-06-02 23:05:43 +0700

    test changes

--- app.js
+++ app.js
@@ -6,25 +6,22 @@ const cors = require("cors");
 const dataRoutes = require("./src/routes/data");
 const errorHandler = require("./src/middleware/errorHandler");
 
-
 const app = express();
 app.use(
   bodyParser.urlencoded({
     extended: true,
-  }),
+  })
 );
 
 app.use(cors());
 app.use(bodyParser.json());
 
-
-app.use('/', dataRoutes);
+app.use("/", dataRoutes);
 
 app.get("/", (req, res) => {
-  res.json({ message: "The server has started." });
+  res.json({ message: "The server has started v1." });
 });
 
 app.use(errorHandler);
-
 
 module.exports = app;
