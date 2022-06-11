96f3c0a302312015d40bb6732ee675cdfb8705d6
plotok11 <fajarfyto11@gmail.com>
2022-06-11 23:21:42 +0700

    Add route get Bestpicks, Recomendation

--- src/controllers/hotel/hotel-data-controller.js
+++ src/controllers/hotel/hotel-data-controller.js
@@ -1,4 +1,6 @@
 const data = require("../../models/hotel-data");
+const _ = require("lodash")
+const dataForBestPick = _.shuffle(data)
 
 module.exports = {
   getAllHotels: async (req, res) => {
@@ -27,6 +29,73 @@ module.exports = {
         status: "success",
         message: "Successfully retrieved!",
         data: filter || data.slice(0, limitReturn),
+      });
+    } catch (err) {
+      return err.message;
+    }
+  },
+
+  getHotelForRecomendation: async (req, res) => {
+    try {
+      // Define Variable Filter
+      const filterByName = req.query.name;
+      const limitReturn = req.query.limit || 15;
+
+      // If empty
+      if (data.length < 1) {
+        return res.status(404).json({
+          status: "error",
+          message: "Hotel Not Found!",
+        });
+      }
+      // Check filter by hotel name
+      if (filterByName) {
+        var filter = data.filter((hotel) =>
+          hotel.namaHotel.toLowerCase().includes(filterByName.toLowerCase())
+        );
+        filter = filter.slice(0, limitReturn);
+      }
+
+      const result = filter || data.slice(0, limitReturn)
+      const shuffle = _.shuffle(result) 
+
+      // Return data
+      return res.json({
+        status: "success",
+        message: "Successfully retrieved!",
+        data: shuffle
+      });
+    } catch (err) {
+      return err.message;
+    }
+  },
+
+  getHotelForBestPick: async (req, res) => {
+    try {
+      // Define Variable Filter
+      const filterByName = req.query.name;
+      const limitReturn = req.query.limit || 15;
+
+      // If empty
+      if (dataForBestPick.length < 1) {
+        return res.status(404).json({
+          status: "error",
+          message: "Hotel Not Found!",
+        });
+      }
+      // Check filter by hotel name
+      if (filterByName) {
+        var filter = dataForBestPick.filter((hotel) =>
+          hotel.namaHotel.toLowerCase().includes(filterByName.toLowerCase())
+        );
+        filter = filter.slice(0, limitReturn);
+      }
+
+      // Return data
+      return res.json({
+        status: "success",
+        message: "Successfully retrieved!",
+        data: filter || dataForBestPick.slice(0, limitReturn),
       });
     } catch (err) {
       return err.message;
