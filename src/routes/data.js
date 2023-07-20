f8df470a4ba8a24a9aa38bc308fb88d08fbdc062
alvigradientvs <129727461+alvigradientvs@users.noreply.github.com>
2023-07-20 21:00:58 +0700

    remove unused file

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const dataController = require("../controllers/hotel/hotel-data-controller");
-const dataflaskController = require("../controllers/model-integration/model-integration-controller");
-const djangoModelIntegration = require("../controllers/model-integration/django-model-integration");
-const RecommendationModelIntegration = require("../controllers/model-integration/model-rekomendasi-controller");
 
 const router = express.Router();
 
@@ -21,18 +18,5 @@ router.get(
 router.get("/hotel-data", dataController.getAllHotels);
 router.get("/hotel-data/:id", dataController.getHotelById);
 
-// Flask Model Integration
-router.get("/list-hotels", dataflaskController.getAllFlaskHotels);
-router.get("/list-hotels/:id", dataflaskController.getFlaskHotelById);
-router.post(
-  "/recommendation",
-  RecommendationModelIntegration.getRecommendation
-);
 
-// Django Model Integration
-router.get("/hotel-bestpicks", djangoModelIntegration.getHotelBestPicks);
-router.get(
-  "/hotel-bestpicks/:id",
-  djangoModelIntegration.getHotelBestPicksByID
-);
 module.exports = router;
